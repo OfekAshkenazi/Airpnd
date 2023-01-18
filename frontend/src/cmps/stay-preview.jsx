8e69ac3534fdd6e786ac64e0cf7d6b4c284056c1
ofek ashkenazi <ofeka18@gmail.com>
2023-01-19 01:22:38 +0200

    add some font and styeling to index

--- frontend/src/cmps/stay-preview.jsx
+++ frontend/src/cmps/stay-preview.jsx
@@ -1,17 +1,25 @@
+import { faStar } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { ImageSlider } from "./image-silder"
 
 export function StayPreview({ stay }) {
-  
+
     return (
         <article className="stay-grid">
             <div className="img-container" style={{ cursor: 'pointer' }} >
                 <ImageSlider stayId={stay._id} images={stay.imgUrls} />
             </div>
-            <div className="stay-small-data">
-                <div className="flex">{stay.loc.city} {stay.loc.country}</div>
+            <div className="stay-small-details grid">
+                <div className="flex">
+                    <div className="loc">
+                        {stay.loc.city} {stay.loc.country}
+                    </div>
+                    <FontAwesomeIcon icon={faStar} />
+                </div>
                 <p>{stay.loc.address}</p>
                 <p>{stay.price} night</p>
             </div>
+
         </article>
     )
 }
