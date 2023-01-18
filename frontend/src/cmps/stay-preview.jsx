630dc8bcef3658b68e2073b8beabfd0827ffeb27
ofek ashkenazi <ofeka18@gmail.com>
2023-01-18 10:17:36 +0200

    fix variables and upgrade preview

--- frontend/src/cmps/stay-preview.jsx
+++ frontend/src/cmps/stay-preview.jsx
@@ -5,7 +5,9 @@ export function StayPreview({ stay }) {
                 <img src={stay.imgUrls[0]} alt="" />
             </div>
             <div className="stay-small-data">
-                <div className="flex">{stay.loc.city}</div>
+                <div className="flex">{stay.loc.city} {stay.loc.country}</div>
+                <p>{stay.loc.address}</p>
+                <p>{stay.price} night</p>
             </div>
         </article>
     )
