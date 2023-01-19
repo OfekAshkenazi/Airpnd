33b50c6ead9fa2acba35d5292cddc84cd5474eeb
ofek ashkenazi <ofeka18@gmail.com>
2023-01-19 15:05:11 +0200

    .

--- frontend/src/store/user.reducer.js
+++ frontend/src/store/user.reducer.js
@@ -1,8 +1,5 @@
 import { userService } from '../services/user.service.js'
 
-export const INCREMENT = 'INCREMENT'
-export const DECREMENT = 'DECREMENT'
-export const CHANGE_COUNT = 'CHANGE_COUNT'
 export const SET_USER = 'SET_USER'
 export const SET_WATCHED_USER = 'SET_WATCHED_USER'
 export const REMOVE_USER = 'REMOVE_USER'
@@ -10,7 +7,6 @@ export const SET_USERS = 'SET_USERS'
 export const SET_SCORE = 'SET_SCORE'
 
 const initialState = {
-    count: 10,
     user: userService.getLoggedinUser(),
     users: [],
     watchedUser : null
@@ -19,15 +15,6 @@ const initialState = {
 export function userReducer(state = initialState, action) {
     var newState = state
     switch (action.type) {
-        case INCREMENT:
-            newState = { ...state, count: state.count + 1 }
-            break
-        case DECREMENT:
-            newState = { ...state, count: state.count - 1 }
-            break
-        case CHANGE_COUNT:
-            newState = { ...state, count: state.count + action.diff }
-            break
         case SET_USER:
             newState = { ...state, user: action.user }
             break
@@ -43,9 +30,7 @@ export function userReducer(state = initialState, action) {
         case SET_USERS:
             newState = { ...state, users: action.users }
             break
-        case SET_SCORE:
-            newState = { ...state, user: { ...state.user, score: action.score } }
-            break
+ 
         default:
     }
     // For debug:
