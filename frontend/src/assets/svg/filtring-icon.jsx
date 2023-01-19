212e2a51304638e8caf9d8457d405fc957c79edc
ofek ashkenazi <ofeka18@gmail.com>
2023-01-19 22:00:17 +0200

    filter-icons

---
+++ frontend/src/assets/svg/filtring-icon.jsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+export function IconFiltering(props) {
+  return (
+    <svg
+      viewBox="0 0 21 21"
+      fill="currentColor"
+      height="20px"
+      width="20px"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M6.5 4a1 1 0 011 1v2a1 1 0 11-2 0V5a1 1 0 011-1zM18.5 6h-11M5.5 6h-3M6.5 14a1 1 0 011 1v2a1 1 0 01-2 0v-2a1 1 0 011-1zM18.5 16h-11M5.5 16h-3M14.5 9a1 1 0 011 1v2a1 1 0 01-2 0v-2a1 1 0 011-1zM13.5 11h-11M18.5 11h-3" />
+      </g>
+    </svg>
+  )
+}
