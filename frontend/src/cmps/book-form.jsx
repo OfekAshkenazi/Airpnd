17638f8391e889d2998d240a662f6c0558bfcab3
Guy Peer <peer.g2@gmail.com>
2023-01-19 14:04:33 +0200

    added reserve form

--- frontend/src/cmps/book-form.jsx
+++ frontend/src/cmps/book-form.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
 
-export function BookingForm({ stay, get }) {
-    return <form>
+export function BookingForm({ stay}) {
+    return <form className="book-form">
         <div className="header">
-           <span> ${stay.price} night</span>
-           <span>star</span>
-           <span>star</span>
-            </div>
+            <span className="price"> ${stay.price} night</span>
+            <span className="reviews">{stay.reviews.length} reviews</span>
+        </div>
         <label htmlFor="checkin">Check-In:</label>
         <input type="date" id="checkin" name="checkin"></input>
         <label htmlFor="checkout">Check-Out:</label>
@@ -15,6 +14,16 @@ export function BookingForm({ stay, get }) {
         <label htmlFor="guests">Number of Guests:</label>
         <input type="number" id="guests" name="guests" min="1"></input>
         <input type="submit" value="Reserve"></input>
+        <p>You Won't be charged yet</p>
+        <div className="prices">
+            <span className="nights"></span><span className="cash">$395</span>
+            <span className="clean"></span><span className="cash">$395</span>
+            <span className="service"></span><span className="cash">$395</span>
+        </div>
+        <div className="total">
+            <span>Total</span>
+            <span>$100</span>
+        </div>
     </form>
 }
 
