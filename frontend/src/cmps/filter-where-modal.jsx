6ac4796d5aa7a5fb83584713599b6b036e7ca2ab
Guy Lazarof <lazarofguy111@gmail.com>
2023-01-23 21:01:04 +0200

    header

---
+++ frontend/src/cmps/filter-where-modal.jsx
@@ -0,0 +1,41 @@
+
+
+export function FilterWhereModal() {
+
+  return (
+    <section className='filter-where-modal'>
+      <div className='common-searches'>
+        <div className='common-search'>
+          <span className='place-icon'>🕗</span>
+          <span>
+            <p className='recent-stay'>Madrid · Stays</p>
+            <p className='recent-time'>Any week</p>
+          </span>
+        </div>
+
+        <div className='common-search'>
+          <span className='place-icon'>🕗</span>
+          <span>
+            <p className='recent-stay'>Metroopolitan City of Naples · Stays</p>
+            <p className='recent-time'>Any week</p>
+          </span>
+        </div>
+
+        <div className='common-search'>
+          <span className='place-icon'>🕗</span>
+          <span>
+            <p className='recent-stay'>North America · Stays</p>
+            <p className='recent-time'>Any week</p>
+          </span>
+        </div>
+
+
+
+      </div>
+
+      <div className='search-by-map'>
+
+      </div>
+    </section>
+  )
+}
