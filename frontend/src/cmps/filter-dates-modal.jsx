e3b69dc00ea0d034bdfa62c104c31bfe4bd384c7
ofek ashkenazi <ofeka18@gmail.com>
2023-01-25 11:29:19 +0200

    .

--- frontend/src/cmps/filter-dates-modal.jsx
+++ frontend/src/cmps/filter-dates-modal.jsx
@@ -27,24 +27,19 @@ export function FilterDatesModal() {
   }
 
   return (
-    <section className='filter-dates-modal'>
-      <div>
-        <p>
-          <DateRange
-            editableDateInputs={true}
-            onChange={(item) => {
-              setOrder([item.selection])
-              handleDateChange(item.selection)
-            }
-            }
-            ranges={order}
-            months={2}
-            direction={'horizontal'}
-            className='date-modal'
-          />
-
-        </p>
-      </div>
-    </section>
+    <div className='filter-dates-modal'>
+      <DateRange
+        editableDateInputs={true}
+        onChange={(item) => {
+          setOrder([item.selection])
+          handleDateChange(item.selection)
+        }
+        }
+        ranges={order}
+        months={2}
+        direction={'horizontal'}
+        className='date-modal'
+      />
+    </div>
   )
 }
