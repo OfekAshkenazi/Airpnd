f7049031e3a8d6f6766cd1b82343f8a6e56ab1a1
Guy Lazarof <lazarofguy111@gmail.com>
2023-01-27 17:20:58 +0200

    add dashboard stays

---
+++ frontend/src/cmps/host-stay-list.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+export function HostStayList() {
+  const user = useSelector(storeState => storeState.userModule.user)
+
+  const [hostStays, setHostStays] = useState([])
+
+  useEffect(() => {
+    onLoadHostStays()
+  }, [])
+
+  async function onLoadHostStays() {
+    console.log('hi')
+  }
+
+  if (!user.isOwner) return <h2>you are not owner</h2>
+  return (
+    <section className='host-stay-list flex'>
+      <div className='host-stay'>
+        {/* <img src="" alt="" /> */}
+        <div>
+          <h4>White City Villa</h4>
+          <p>1901 thornoids sdiof sadisod </p>
+        </div>
+        <div>
+          <p>for rent</p>
+        </div>
+        <div>
+          56
+          56
+          |
+          56
+          <div>
+            <span>
+              <p>
+                $48,000
+              </p>
+              <p>/year</p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='host-stay'>hello</div>
+    </section>
+  )
+}
