a62bd75a00f42d388daf3c06747b97c347503a23
ofek ashkenazi <ofeka18@gmail.com>
2023-01-28 18:53:50 +0200

    working on scsss before last demo

--- frontend/src/cmps/host-order-preview.jsx
+++ frontend/src/cmps/host-order-preview.jsx
@@ -2,27 +2,22 @@
 export function HostOrderPreview({ handelSelectChange, order }) {
 
     return (
-        <div className="order-status align-center flex">
-            <div className="orders-left-side align-center flex">
-                <div className="img-username flex align-center">
-                    <div className="user-img-order-container">
-                        <img src={order.byUser.imgUrl} alt="" />
-                    </div>
-                    <p>{order.byUser.fullname}</p>
-                </div>
-                <p className="type">{order.stay.type}</p>
+        <div className="order-status">
+            <p className="user-img-order-container">
+                <img src={order.byUser.imgUrl} alt="" />
+                {order.byUser.fullname}
+            </p>
+            <p className="type">{order.stay.type}</p>
+            <p>{order.startDate.slice(0, 10)}</p>
+            <p className="price-host-order">{order.totalPrice}</p>
+            <div className="status-indicator align-center flex">
+                <select onChange={(event) => handelSelectChange(event, order._id)} name="status" id="">
+                    <option value={order.status}>{order.status}</option>
+                    <option value="approve">Approve</option>
+                    <option value="decline">Decline</option>
+                </select>
             </div>
-            <div className="order-right-side align-center flex">
-                <p>{order.startDate.slice(0, 10)}</p>
-                <p>{order.totalPrice}</p>
-                <div className="status-indicator align-center flex">
-                    <select onChange={(event) => handelSelectChange(event, order._id)} name="status" id="">
-                        <option value={order.status}>{order.status}</option>
-                        <option value="approve">Approve</option>
-                        <option value="decline">Decline</option>
-                    </select>
-                </div>
-            </div>
+           
         </div>
     )
 }
