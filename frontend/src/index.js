3bb86b8db69b1dc874aa11ea920982df50d8aa10
ofek ashkenazi <ofeka18@gmail.com>
2023-01-29 01:36:50 +0200

    deploy?

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { store } from './store/store'
 import { RootCmp } from './root-cmp'
 import './assets/styles/main.scss'
@@ -19,4 +19,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+// serviceWorkerRegistration.unregister();
