c6952d847821d8cf428126241f85dd0f1915c28e
Guy Lazarof <lazarofguy111@gmail.com>
2023-01-30 00:04:57 +0200

    scss

--- frontend/src/cmps/host-order-preview.jsx
+++ frontend/src/cmps/host-order-preview.jsx
@@ -20,15 +20,18 @@ export function HostOrderPreview({ handelSelectChange, order }) {
             <p className="mobile-gone">{order.startDate.slice(0, 10)}</p>
             <p className="price-host-order">$ {order.totalPrice}</p>
             <div className="status-indicator align-center flex column">
-                <select onChange={(event) => handelSelectChange(event, order._id)} name="status" id="">
-                    <option value={order.status}>{order.status}</option>
-                    <option value="approve">Approve</option>
-                    <option value="decline">Decline</option>
-                </select>
+                <button onClick={() => handelSelectChange('approve', order._id)} className={order.status === 'approve' ? 'approve active' : 'approve'} >Approve</button>
+                <button onClick={() => handelSelectChange('decline', order._id)} className={order.status === 'decline' ? 'decline active' : 'decline'} >Decline</button>
+
             </div>
         </div>
     )
 }
+{/* <select onChange={(event) => handelSelectChange(event, order._id)} name="status" id="">
+    <option value={order.status}>{order.status}</option>
+    <option value="approve">Approve</option>
+    <option value="decline">Decline</option>
+</select> */}
 
 {/* <button onClick={() => handelSelectChange('approve', order._id)} className={order.status === 'approve' ? 'approve active' : 'approve'} >Approve</button>
 <button onClick={() => handelSelectChange('decline', order._id)} className={order.status === 'decline' ? 'decline active' : 'decline'} >Decline</button> */}
