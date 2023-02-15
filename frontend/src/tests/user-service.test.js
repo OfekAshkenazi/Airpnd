aaf822a92a48fef75ef56dce3741eb82ce8a1d00
Guy Peer <peer.g2@gmail.com>
2023-02-15 15:13:55 +0200

    tests

---
+++ frontend/src/tests/user-service.test.js
@@ -0,0 +1,32 @@
+import { httpService } from '../services/http.service.js'
+import { userService } from '../services/user.service'
+
+jest.mock('../services/http.service.js')
+
+const mockUser = {
+  fullname: "guy peer",
+  imgUrl: "https://i.postimg.cc/gJ8DDKxF/peer.png",
+  isOwner: false,
+  wishList: [],
+  _id: "63cfe9f88276fe4e2c861da6"
+}
+
+const mockCred = { username: mockUser.username, password: 'sk' }
+
+describe('UserService', () => {
+
+  beforeEach(() => {
+    httpService.post.mockReset()
+  })
+
+  it('should login successfully', async () => {
+    expect.assertions(2)
+
+    const httpResp = mockUser
+    httpService.post.mockResolvedValue(httpResp)
+    const res = await userService.login(mockCred)
+    expect(res).toStrictEqual(mockUser)
+    expect(httpService.post).toBeCalled()
+  })
+
+})
