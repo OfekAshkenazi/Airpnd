1c5b16499352631c390ba3869baa7475920ec190
ofek ashkenazi <ofeka18@gmail.com>
2023-03-13 15:42:41 +0200

    change classname for edit page

--- frontend/src/cmps/stay/edit-stay.jsx
+++ frontend/src/cmps/stay/edit-stay.jsx
@@ -2,11 +2,21 @@ export function EditStay() {
     return (
         <section className="edit-stay">
             <section className="images-area">
-                <div className="grid-img-1"></div>
-                <div className="grid-img-2"></div>
-                <div className="grid-img-3"></div>
-                <div className="grid-img-4"></div>
-                <div className="grid-img-5"></div>
+                <div className="grid-img-1">
+
+                </div>
+                <div className="grid-img-2">
+
+                </div>
+                <div className="grid-img-3">
+
+                </div>
+                <div className="grid-img-4">
+
+                </div>
+                <div className="grid-img-5">
+
+                </div>
             </section>
         </section>
     )
