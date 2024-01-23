0b8ff467924f0532454f5321897442d1f0da1c9c
ofek ashkenazi <ofeka18@gmail.com>
2024-01-23 10:19:40 +0200

    added port 3030 to developer mode. and download new version of chart.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origin: ['http://127.0.0.1:3000', 'http://localhost:3000'],
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3030','http://localhost:3000'],
         credentials: true
     }
     app.use(cors(corsOptions))
