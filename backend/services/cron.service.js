f881c6ebede181c60642cbded29d57f8a764f257
ofek ashkenazi <ofeka18@gmail.com>
2024-04-08 11:19:49 +0300

    added cron

---
+++ backend/services/cron.service.js
@@ -0,0 +1,22 @@
+const cron = require('cron')
+const https = require('cron')
+
+const URL = "https://airpnd.onrender.com"
+
+const job = new cron.CronJob("*/14 * * * *", function () {
+    https.get(URL,(res) => {
+        if(res.statusCode = 200) {
+            console.log("Get request success")
+        }else {
+            console.log("Get request fail: ", res.statusCode)
+
+        }
+
+    }).on("error",(e) => {
+        console.log("Error while sending request: ", e)
+    })
+})
+
+
+
+export default job
