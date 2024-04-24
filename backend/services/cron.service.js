6954ed646342420b4fd4ba4dec724ef8bb0cf9d1
ofek ashkenazi <ofeka18@gmail.com>
2024-04-24 19:11:53 +0300

    got off cron job.

--- backend/services/cron.service.js
+++ backend/services/cron.service.js
@@ -1,22 +1,22 @@
-const cron = require('cron')
-const https = require('https')
+// const cron = require('cron')
+// const https = require('https')
 
-const URL = "https://airpnd.onrender.com"
+// const URL = "https://airpnd.onrender.com"
 
-const job = new cron.CronJob("*/14 * * * *", function () {
-    https.get(URL,(res) => {
-        if(res.statusCode = 200) {
-            console.log("Get request success")
-        }else {
-            console.log("Get request fail: ", res.statusCode)
+// const job = new cron.CronJob("*/14 * * * *", function () {
+//     https.get(URL,(res) => {
+//         if(res.statusCode = 200) {
+//             console.log("Get request success")
+//         }else {
+//             console.log("Get request fail: ", res.statusCode)
 
-        }
+//         }
 
-    }).on("error",(e) => {
-        console.log("Error while sending request: ", e)
-    })
-})
+//     }).on("error",(e) => {
+//         console.log("Error while sending request: ", e)
+//     })
+// })
 
 
 
-module.exports = job
+// module.exports = job
